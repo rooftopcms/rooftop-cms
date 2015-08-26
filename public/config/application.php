112b219a74756fb8ff870c7a88c8f0a3a18c8a7f
Ed Jones <ed@errorstudio.co.uk>
2015-08-26 17:41:18 +0100

    Include multisite defines

--- public/config/application.php
+++ public/config/application.php
@@ -68,6 +68,13 @@ define('DISALLOW_FILE_EDIT', true);
 
 // Allow multisite
 define( 'WP_ALLOW_MULTISITE', true );
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', true);
+define('DOMAIN_CURRENT_SITE', 'hosted_wordpress.dev');
+//define('PATH_CURRENT_SITE', '/');
+$base = "/";
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
 
 /**
  * Bootstrap WordPress

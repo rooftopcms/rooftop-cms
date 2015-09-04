f2375d89f1e205afa0f95b46c884705231fa1c34
Paul Hendrick <paul@errorstudio.co.uk>
2015-09-04 18:04:53 +0100

    added predis dependency

--- public/web/wp-config.php
+++ public/web/wp-config.php
@@ -4,6 +4,7 @@
  * This file is required in the root directory so WordPress can find it.
  * WP is hardcoded to look in its own directory or one directory up for wp-config.php.
  */
-require_once(dirname(__DIR__) . '/vendor/autoload.php');
+define("VENDOR_PATH", dirname(__DIR__).'/vendor/');
+require_once(VENDOR_PATH . 'autoload.php');
 require_once(dirname(__DIR__) . '/config/application.php');
 require_once(ABSPATH . 'wp-settings.php');

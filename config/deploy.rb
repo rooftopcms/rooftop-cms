0fd779017a03de730582fe23d7f223ba39db0faf
Ed Jones <ed@errorstudio.co.uk>
2015-11-18 14:15:28 +0000

    Add cap git submodule support

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,33 +28,5 @@ set :local_db_name, "rooftop_cms"
 #CORS required
 set :include_nginx_cors, true
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
+# Include submodules. This requires the capistrano-git-submodule-strategy gem.
+set :git_strategy, Capistrano::Git::SubmoduleStrategy

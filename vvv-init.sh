cf7cdd26b341a0e1b2284bc80b49d0156b009464
Ed Jones <ed@errorstudio.co.uk>
2016-01-04 15:03:11 +0000

    Set path to wp when running init in development

--- vvv-init.sh
+++ vvv-init.sh
@@ -11,7 +11,8 @@ if [ ! -d "web/wp/wp-admin" ]; then
 	echo "Creating database (if it's not already there)"
 	mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
 	mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'$DB_HOST' IDENTIFIED BY '$DB_PASSWORD';"
-	wp core install --url=$WP_URL --title="Hosted Wordpress" --admin_user=admin --admin_password=password --admin_email=hosting@errorstudio.co.uk --allow-root
+	wp core install --url=$WP_URL --title="Hosted Wordpress" --admin_user=admin --admin_password=password
+	--admin_email=hosting@errorstudio.co.uk --allow-root --path=public/web/wp
 	# wp theme delete twentythirteen --allow-root; wp theme delete twentyfourteen --allow-root; wp theme delete twentyfifteen --allow-root; wp plugin delete hello --allow-root; wp plugin delete aksimet --allow-root;
 	
 	cd -

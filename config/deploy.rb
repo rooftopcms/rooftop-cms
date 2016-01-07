c353963b5abbcaddfdb882c5d99141339a5b0442
Paul Hendrick <paul@errorstudio.co.uk>
2016-01-07 16:00:26 +0000

    set the nginx root to the WP path. remove rewrite rules pointing to the old root

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,17 +4,17 @@ set :repo_url, 'ssh://git@github.com/rooftopcms/rooftop-cms'
 set :primary_domain, "rooftopcms.io"
 
 # The folder which will be the http root in nginx
-set :http_root, "/current/public/web"
+set :http_root, "/current/public/web/wp"
 
 # Peculiar to bedrock, the uploads folder is in a non-standard place.
 # You don't need to set this for normal wordpress.
 set :uploads_folder, 'public/web/app/uploads'
 
 # Custom nginx rules for bedrock
-set :custom_nginx_rules, [
-      "rewrite ^/(wp-.*.php)$ /wp/$1 last;",
-      "rewrite ^/(wp-(content|admin|includes).*) /wp/$1 last;"
-]
+#set :custom_nginx_rules, [
+#      "rewrite ^/(wp-.*.php)$ /wp/$1 last;",
+#      "rewrite ^/(wp-(content|admin|includes).*) /wp/$1 last;"
+#]
 
 #development settings
 set :local_domain, "rooftop-cms.dev"

5af4a57bad28c81fa313134023f6cde972fe0ad7
Ed Jones <ed@errorstudio.co.uk>
2016-01-07 16:33:32 +0000

    don't force ssl - we'll do that on cloudflare

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -38,7 +38,7 @@ set :ssl_dir, File.join(File.dirname(__FILE__),"ssl")
 set :ssl_cert, "rooftopcms.io.public.crt"
 set :ssl_key, "rooftopcms.io.private.key.gpg" #this should be a gpg-encrypted key
 set :ssl_dh, "rooftopcms.io.dh.pem.gpg" #this should be a gpg-encrypted key
-set :force_ssl, true #redirect all non-ssl requests to ssl
+#set :force_ssl, true #redirect all non-ssl requests to ssl
 
 #http basic auth
 set :basic_auth_required, false

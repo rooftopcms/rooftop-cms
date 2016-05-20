8317521a140418ab63569f6d8b03e9d4b1976fc5
Ed Jones <ed@errorstudio.co.uk>
2016-05-20 12:36:27 +0100

    staging stage => qa

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,13 +1,13 @@
 # Server configuration. For a simple site this is just one entry.
 role :app, [
-    "deployment@rooftop-staging.vm.errorstudio.com"
+    "deployment@rooftop-qa.vm.errorstudio.com"
 ]
 
 role :web, [
-    "deployment@rooftop-staging.vm.errorstudio.com"
+    "deployment@rooftop-qa.vm.errorstudio.com"
 ]
 
-role :db, %w{deployment@rooftop-staging.vm.errorstudio.com}
+role :db, %w{deployment@rooftop-qa.vm.errorstudio.com}
 
 # Git branch
 set :branch, 'development'
@@ -19,29 +19,29 @@ set :base_domain, "prelaunch.errorstudio.com"
 set :prelaunch_domain, ->{"#{fetch(:primary_domain)}.#{fetch(:base_domain)}"}
 
 # set the deploy domain to the prelaunch domain
-set :deploy_domain, 'staging.rooftopcms.io'
+set :deploy_domain, 'qa.rooftopcms.io'
 
 #redirects domains to the primary domain as a 301
 set :domain_redirects, %w()
 
 #domains which this site will answer to (i.e. not redirect)
-set :site_domains, [fetch(:deploy_domain), "~^.*\.staging\.rooftopcms\.io"]
+set :site_domains, [fetch(:deploy_domain), "~^.*\.qa\.rooftopcms\.io"]
 
 #rewrites in nginx format - useful for specifying hard-coded urls for redirection after launch
 set :url_rewrites, {}
 
 #http basic auth
 set :basic_auth_required, true
-set :basic_auth_username, 'staging'
-set :basic_auth_password, 'staging'
+set :basic_auth_username, 'rooftopqa'
+set :basic_auth_password, 'rooftopqa'
 
 # Wordpress settings
-set :db_host, `source public/.env.staging; echo $DB_HOST`.strip
-set :db_prefix, `source public/.env.staging; echo $DB_PREFIX`.strip
+set :db_host, `source public/.env.qa; echo $DB_HOST`.strip
+set :db_prefix, `source public/.env.qa; echo $DB_PREFIX`.strip
 
 # Custom env vars for Rooftop
 set :custom_env_vars, {
-    "REDIS_HOST" => `source public/.env.staging; echo $REDIS_HOST`.strip,
-    "REDIS_PORT" => `source public/.env.staging; echo $REDIS_PORT`.strip,
-    "REDIS_DB" => `source public/.env.staging; echo $REDIS_DB`.strip,
+    "REDIS_HOST" => `source public/.env.qa; echo $REDIS_HOST`.strip,
+    "REDIS_PORT" => `source public/.env.qa; echo $REDIS_PORT`.strip,
+    "REDIS_DB" => `source public/.env.qa; echo $REDIS_DB`.strip,
 }
